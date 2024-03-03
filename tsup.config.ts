5e1c742cddb74d82be50c3bf7179b271cd98a62f
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2024-03-04 02:08:22 +0800

    refactor!: drop node 16

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   entry: ['./src/{index,cli}.ts'],
   format: ['esm'],
-  target: 'node16.14',
+  target: 'node18',
   clean: true,
   dts: true,
 })
