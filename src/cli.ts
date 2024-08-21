e0ebdcada6530f4490e41c0f0bc7f47bbd911e67
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2024-08-21 14:34:29 +0800

    refactor: replace commander with cac

--- src/cli.ts
+++ src/cli.ts
@@ -1,13 +1,18 @@
-import { program } from 'commander'
+import cac from 'cac'
 import consola from 'consola'
+import { version } from '../package.json'
 import { config, run } from '.'
 
-program
-  .argument('[projectPath]', 'project path')
+const cli = cac('@sxzz/create')
+
+cli
+  .command('[projectPath]', 'create a project')
   .action((projectPath?: string) => {
     run(projectPath).catch((error) => error && consola.error(error))
   })
-program
+cli
   .command('config')
   .action(() => config().catch((error) => consola.error(error)))
-program.parse()
+cli.help()
+cli.version(version)
+cli.parse()
