038d1e0f0fb1143b0337fea8b75cb019730a99ba
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-05-10 00:31:21 +0800

    chore: upgrade deps

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'tsdown'
 
 export default defineConfig({
-  entry: ['./src/{index,cli}.ts'],
-  format: ['esm'],
-  target: 'node18',
-  clean: true,
-  dts: { resolve: ['@antfu/utils', 'replace-in-file'] },
+  entry: './src/{index,cli}.ts',
+  dts: {
+    resolve: ['@antfu/utils', 'replace-in-file'],
+  },
   unused: { level: 'error' },
   alias: {
     chalk: 'ansis',
