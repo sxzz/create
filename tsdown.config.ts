c859b0e5ce102dc3dfff496217fb901f64a7eb2b
Kevin Deng <sxzz@sxzz.moe>
2026-02-08 21:42:00 +0800

    chore: upgrade deps

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,27 +1,27 @@
-import { defineConfig } from 'tsdown'
+import { nodeLib } from 'tsdown-preset-sxzz'
 
-export default defineConfig({
-  entry: './src/{index,cli}.ts',
-  dts: {
-    resolve: ['@antfu/utils', 'replace-in-file'],
+export default nodeLib(
+  {
+    entry: ['./src/{index,cli}.ts'],
+    inlineDeps: ['@antfu/utils', 'replace-in-file'],
   },
-  unused: {
-    level: 'error',
-    ignore: ['tinyglobby'],
-  },
-  plugins: [
-    {
-      name: 'alias',
-      resolveId(id) {
-        if (id === 'glob') {
-          return { id: 'tinyglobby', external: true }
-        }
-        if (id === 'chalk') {
-          return { id: 'ansis', external: true }
-        }
-      },
+  {
+    unused: {
+      level: 'error',
+      ignore: ['tinyglobby'],
     },
-  ],
-  exports: true,
-  inlineOnly: ['@antfu/utils', 'replace-in-file'],
-})
+    plugins: [
+      {
+        name: 'alias',
+        resolveId(id) {
+          if (id === 'glob') {
+            return { id: 'tinyglobby', external: true }
+          }
+          if (id === 'chalk') {
+            return { id: 'ansis', external: true }
+          }
+        },
+      },
+    ],
+  },
+)
